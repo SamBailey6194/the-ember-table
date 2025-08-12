9ef6f4b7364425334dcbcb6d929c175165a6d278
SamBailey6194 <samabailey6194@gmail.com>
2025-08-12 10:50:48 +0100

    Fix bug where tailwind classes weren't changing the color

--- the_ember_table/settings.py
+++ the_ember_table/settings.py
@@ -198,12 +198,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
 NPM_BIN_PATH = (
     os.getenv("NPM_BIN_PATH")
     )
-TAILWIND_CSS_PATH = 'static/css/style.css'
+TAILWIND_CSS_PATH = 'css/style.css'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
