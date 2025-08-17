0658be4eaae706d3a2e59acc38bdd86b360847c2
SamBailey6194 <samabailey6194@gmail.com>
2025-08-17 17:19:50 +0100

    Create logout modal and it functions

--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.utils.dateparse import parse_date
-from .forms import CancelBookingForm
 from .models import Booking, Table, Customer
 
 
